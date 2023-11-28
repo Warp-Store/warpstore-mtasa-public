8f32f7ba2464bdfac1ee2ba7d285edbe594a0426
Rabbit <personal.danielsilva.d3v@outlook.com>
2023-11-27 22:12:20 -0300

    Add files via upload

--- config.lua
+++ config.lua
@@ -1,5 +1,5 @@
 config = {
-    token = 'mtasa-71f1daba-9bf0-4bb8-ae06-9c29cc392541-1531edcb-c25e-4a59-9e42-d54ae5dd979a', -- Você pegar seu token em: https://docs.warpstore.app
+    token = 'seu-token', -- Você pegar seu token em: https://docs.warpstore.app
     chat = {
         global = false, -- (true) para todos os jogadores ver, ou (false) para somente o jogador!
         color = '#ffffff', -- (#) Cor da mensagem que será enviada no chat!
@@ -22,6 +22,17 @@ config = {
     cooldownRunCommand = 2500, -- Tempo de rodar os comandos de entrega depois de pagos!
     cooldownExpire = 1000, -- Tempo de verificação para expirar o grupo do usuário!
     webhook = "webhook-url", -- Link da webhook do discord, caso você não tenha o acesso ao plano que permite o bot, você pode usar a webhook!,
+
+    findUserById = function(id)
+        v = false
+        for i, player in ipairs (getElementsByType("player")) do
+            if getElementData(player, "ID") == id then
+                v = player
+                break
+            end
+        end
+        return v
+    end,
     
     InfoBox = {
         displaySeconds = 3000, -- Tempo de notificação, caso não for usar info-box não mecha.

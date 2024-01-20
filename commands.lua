0b0bb11fec2af0498090f1d16e1d2d5fff06684f
Rabbit <personal.danielsilva.d3v@outlook.com>
2024-01-20 20:41:19 -0300

    save

--- commands.lua
+++ commands.lua
@@ -12,5 +12,15 @@ commands = {
         local playerAccount = getAccountName( getPlayerAccount( player ))
 
         HSVIP:givePremium(playerAccount, vipName, vipDays, 'Dias', 'VIP:0' .. vipNumber)
+    end,
+    ["hyperscripts:addCoin"] = function (args)
+        local playerID = tonumber(args[1]);
+        local elementData = args[2]
+        local quantityCoin = tonumber(args[3]);
+        
+        local player = getPlayerID(playerID);
+        local currentCoins = getElementData(player, elementData) or 0;
+
+        setElementData(player, elementData, currentCoins + quantityCoin)
     end
 }

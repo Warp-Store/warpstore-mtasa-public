8f1a70ec901327b9dde50bfcb56672058b4384f5
Maicon-Developer <maicon16novinho38@gmail.com>
2024-06-19 21:44:02 -0300

    🌴Atualização.

--- commands.lua
+++ commands.lua
@@ -1,19 +1,18 @@
 commands = {
-    ["hyperscripts:addVip"] = function (args)
+    ['hyperscripts:addVip'] = function (args)
         local playerID = tonumber(args[1]);
-        local exportResourceName = args[2]
-        local vipName = args[3]
-        local vipNumber = args[4]
-        local vipDays = tonumber(args[5])
+        local vipName = args[2]
+        local vipNumber = args[3]
+        local vipDays = tonumber(args[4])
 
-        local HSVIP = exports[exportResourceName]
+        local HSVIP = exports['[HS]VIP_System']
 
         local player = getPlayerID(playerID);
         local playerAccount = getAccountName( getPlayerAccount( player ))
 
-        HSVIP:givePremium(playerAccount, vipName, vipDays, 'Dias', 'VIP:0' .. vipNumber)
+        HSVIP:givePremium(playerAccount, vipName, vipDays, 'Dias', 'VIP:0'..vipNumber)
     end,
-    ["hyperscripts:addCoin"] = function (args)
+    ['hyperscripts:addCoin'] = function (args)
         local playerID = tonumber(args[1]);
         local elementData = args[2]
         local quantityCoin = tonumber(args[3]);
@@ -23,37 +22,46 @@ commands = {
 
         setElementData(player, elementData, currentCoins + quantityCoin)
     end,
-    ["hyperscripts:addVehicle"] = function (args)
+    ['hyperscripts:giveItem'] = function (args)
         local playerID = tonumber(args[1]);
-        local exportsResource = args[2]
-        local vehicle = string.gsub(args[3], "-", " ")
+        local item = args[2]
+        local quantity = tonumber(args[3]);
         
         local player = getPlayerID(playerID);
-        local conce = exports[exportsResource];
+        local inventory = exports['[HS]Hyper_Inventory'];
 
-        local vehicleConfig = conce:getVehicleVipConfig(vehicle);
-
-        local verify = conce:getVehicle( account, vehicleName )
-        if not verify then
-            local plate = conce:generatePlate( )
-            conce:setVehicleConce( player, { name = vehicle, model = vehicleConfig.model }, { 255, 255, 255 }, vehicleConfig.category )
+        if not inventory:giveItem( player, item, quantity ) then
+            return config.InfoBox.execute(player, 'Você não possui espaço suficiente para receber o item '..item..' contate a administração.', config.InfoBox.status.warn)
         end
     end,
-    ["hyperscripts:addVehicleVip"] = function (args)
+    ['hyperscripts:addVehicle'] = function (args)
         local playerID = tonumber(args[1]);
-        local exportsResource = args[2]
-        local vehicle = string.gsub(args[3], "-", " ")
-        local validate = tonumber(args[4])
+        local vehicle = string.gsub(args[2], '-', ' ');
+        local vehicleCategory = string.gsub(args[3], '-', ' ');
+        local vehicleModel = tonumber(args[4]);
         
         local player = getPlayerID(playerID);
-        local conce = exports[exportsResource];
+        local conce = exports['[HS]Dealership_System'];
+
+        local verify = conce:getVehicle( account, vehicleName )
+        if not verify then
+            conce:setVehicleConce( player, { ['name'] = vehicle, ['model'] = vehicleModel }, { 255, 255, 255 }, vehicleCategory )
+        end
+    end,
+    ['hyperscripts:addVehicleVip'] = function ( args )
+        local playerID = tonumber(args[1]);
+        local vehicle = string.gsub(args[2], '-', ' ')
+        local validate = tonumber(args[3])
+        
+        local player = getPlayerID(playerID);
+        local conce = exports['Dealership_System'];
 
         local vehicleConfig = conce:getVehicleVipConfig(vehicle);
 
         local verify = conce:getVehicle( account, vehicleName )
         if not verify then
             local plate = conce:generatePlate( )
-            conce:setVehicleVip( player, vehicle, vehicleConfig.category, vehicleConfig.model, validate )
+            conce:setVehicleVip( player, vehicle, conce:generatePlate( ), vehicleConfig.category, vehicleConfig.model, validate )
         end
     end
 }
--- config.lua
+++ config.lua
@@ -1,34 +1,34 @@
 config = {
-    token = 'mtasa-18e8891e-59a8-4364-8d0c-d87d3f1afbc4-59613c4c-cf51-4047-8ce8-3e82a8af5dd6', -- Você pegar seu token em: https://docs.warpstore.app
+    token = 'mtasa-fb66df4b-6870-4fc7-88ac-1feb0cd9e1a1-48e85932-1e91-4f6d-9854-d6f710541dab', -- Você pegar seu token em: https://docs.warpstore.app
     chat = {
         global = false, -- (true) para todos os jogadores ver, ou (false) para somente o jogador!
         color = '#ffffff', -- (#) Cor da mensagem que será enviada no chat!
     },
     commands = {
-        showPurchases = "compras" -- O comando usado para ver as compras e a data de expiração.
+        showPurchases = 'compras' -- O comando usado para ver as compras e a data de expiração.
     },
     panelHtml = {
         enabled = true, -- Caso estiver ativo ele exibe um painel na tela para todos os jogadores avisando que o usuário comprou o produto,
         global = true, -- Caso qusier que todos os jogadores vejam esta mensagem deixe ativado, e false para quando quiser que apenas o comprador veja
-        title = "Warp Delivery",
-        message = ":name fez uma doação do produto :product para o servidor."
+        title = 'Warp Delivery',
+        message = ':name fez uma doação do produto :product para o servidor.'
     },
     messages = {
-        newBuy = "[WARP - DELIVERY] :name comprou :product com sucesso!",
-        notFound = "[WARP - DELIVERY] Não foi encontrado nenhum benefício!",
-        expireIn = "[WARP - DELIVERY] O produto :name e o :item expira em :date",
-        expire = "[WARP - DELIVERY] Ei :name, o seu produto :product expirou e foi removido!", -- item representa caso seja um grupo, quantidade, ou qualquer outro argumento. Não remova
-        refund = "[WARP - DELIVERY] Ei :name, você recebeu o reembolso de uma compra e o :product foi removido de sua conta!", -- quando algum item é reembolsado.
-        nothingToExpire = "[WARP - DELIVERY] Você não tem nenhum produto para expirar", -- item representa caso seja um grupo, quantidade, ou qualquer outro argumento. Não remova
+        newBuy = '[WARP - DELIVERY] :name comprou :product com sucesso!',
+        notFound = '[WARP - DELIVERY] Não foi encontrado nenhum benefício!',
+        expireIn = '[WARP - DELIVERY] O produto :name e o :item expira em :date',
+        expire = '[WARP - DELIVERY] Ei :name, o seu produto :product expirou e foi removido!', -- item representa caso seja um grupo, quantidade, ou qualquer outro argumento. Não remova
+        refund = '[WARP - DELIVERY] Ei :name, você recebeu o reembolso de uma compra e o :product foi removido de sua conta!', -- quando algum item é reembolsado.
+        nothingToExpire = '[WARP - DELIVERY] Você não tem nenhum produto para expirar', -- item representa caso seja um grupo, quantidade, ou qualquer outro argumento. Não remova
         useInfoBox = false, -- Caso ele seja false as mensagens serão exibidas no chat, e caso seja true será exibida na sua info-box
     },
     cooldownRequest = 10000, -- Tempo em milisegundos que o site verifica se tem alguma compra para ativar no jogo.
-    cooldownExpire = 60000, -- Tempo de verificação para expirar o grupo do usuário!
 
-    findUserById = function(id)
+    findUserById = function( id )
         v = false
-        for i, player in ipairs (getElementsByType("player")) do
-            if getElementData(player, "ID") == id then
+        for i, player in ipairs( getElementsByType( 'player' ) ) do
+            local idAlvo = getElementData( player, 'ID' )
+            if idAlvo and tonumber( idAlvo ) == id then
                 v = player
                 break
             end
@@ -40,16 +40,16 @@ config = {
         displaySeconds = 3000, -- Tempo de notificação, caso não for usar info-box não mecha.
 
         -- Aqui fica sua função de info-box que será chamada quando nescessário pelo nosso plugin!
-        execute = function (element, text, type)
-            exports["wp_infobox"]:addNotification(element, text, type)
+        execute = function ( element, text, type )
+            exports['[HS]Notify_System']:notify( element, text, type )
         end,
 
         -- Define aqui os status de sua infobox, não mecha no primeiro valor, apenas no segundo.
         status = {
-            ["success"] = "success",
-            ["info"] = "info",
-            ["warning"] = "warning",
-            ["error"] = "error"
+            ['success'] = 'success',
+            ['info'] = 'info',
+            ['warning'] = 'warning',
+            ['error'] = 'error'
         } 
     }
 }
